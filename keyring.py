313e3e9221134df4bdfafa99063f4eae517fc69a
anfractuosity <anfractuosity@anfractuosity.com>
2022-06-20 11:49:13 +0100

    Fix bugs

--- keyring.py
+++ keyring.py
@@ -6,29 +6,38 @@ from FreeCAD import Base
 from PIL import Image
 
 def test(image, outputprefix, drillA=True):
+    rheight = 80
+    rwidth = 30
+    thick = 2
+
+    x0 = 2
+    y0 = 12
+    x1 = rwidth - 2
+    y1 = rheight - 2
+
+    h1 = y1 - y0
+    w1 = x1 - x0
+
+    hole = 0.2
+    scale = 2.0
+
+    height = int((y1 - y0)*scale) + 1
+    width = int((x1 - x0)*scale) + 1
+
     img = Image.open(image)
     img = img.convert('L')
-    image = np.array(img) > 0
-    image = np.swapaxes(image, 0, 1)
+    arr = np.array(img) > 0
+    arr = np.swapaxes(arr, 0, 1)
+
+    iwidth = ((width-1) // 2) + 1
+    iheight = ((height-1) // 2) + 1
+    print(f"Please use image of size - {iwidth}x{iheight}")
+
+    np.random.seed(1)
+    arr2 = np.random.randint(2, size=(iheight, iwidth)) > 0
 
     doc = FreeCAD.newDocument()
     myPart = doc.addObject("Part::Feature","myPartName")
-    off = 2
-    dim = 1
-
-    rheight = 80
-    rwidth = 30
-    scale = 1.5
-    height = int((rheight) * scale)
-    width = int((rwidth) * scale)
-
-    arr = np.full((height, width), False, dtype=bool)
-    for y in range(height):
-        arr[y][width // 2] = True
-    arr = image
-
-    np.random.seed(1)
-    arr2 = np.random.randint(2, size=(height, width)) > 0
 
     A = np.full((2, 2), False, dtype=bool)
     A[0][0] = True
@@ -38,10 +47,8 @@ def test(image, outputprefix, drillA=True):
     B[0][1] = True
     B[1][0] = True
 
-    thick = 2
-    off = 0 #0.15 + 3
     cube = FreeCAD.ActiveDocument.addObject("Part::Feature", "myCube")
-    cube.Shape = Part.makeBox(rheight - (2*(((1/(height)) * rheight))) + (off), rwidth - (2*(((1/(width)) * rwidth))) + (off), thick)
+    cube.Shape = Part.makeBox(rheight, rwidth, thick)
 
     chmfr = FreeCAD.ActiveDocument.addObject("Part::Fillet", "myChamfer")
     chmfr.Base = FreeCAD.ActiveDocument.myCube
@@ -60,17 +67,10 @@ def test(image, outputprefix, drillA=True):
     SocketSketch.MapMode = 'FlatFace'
     doc.recompute()
 
-    for y in range(0,height,1):
-        for x in range(0,width,1):
-
-            if x < 3 or x >= width - 3 - 1:
-                continue
-
-            if y < 12 or y >= height - 3 - 1:
-                continue
-
-            color = arr[y][x]
-            choice = arr2[y][x]
+    for y in range(0, height):
+        for x in range(0, width):
+            color = arr[y//2][x//2]
+            choice = arr2[y//2][x//2]
             x_ = x % 2
             y_ = y % 2
 
@@ -87,9 +87,11 @@ def test(image, outputprefix, drillA=True):
                 drillb = drill
 
             if (drillA and drilla) or ((not drillA) and drillb):
-                SocketSketch.addGeometry(Part.Circle(App.Vector(((y/(height)) * rheight) + (off/2), ((x/(width)) * rwidth) + (off/2), 0), App.Vector(0,0,1), 0.2),False)
+                y_ = y0 + ((y/(height-1))*h1)
+                x_ = x0 + ((x/(width-1))*w1)
+                SocketSketch.addGeometry(Part.Circle(App.Vector(y_, x_, 0), App.Vector(0,0,1), hole), False)
 
-    SocketSketch.addGeometry(Part.Circle(App.Vector(4, (rwidth - (2*(((1/(width)) * rwidth))) + (off)) / 2, 0), App.Vector(0,0,1), 1.5),False)
+    SocketSketch.addGeometry(Part.Circle(App.Vector(4, rwidth / 2, 0), App.Vector(0,0,1), 1.5),False)
 
     pocket = doc.Body001.newObject("PartDesign::Pocket","Pocket0")
     pocket.Profile = SocketSketch

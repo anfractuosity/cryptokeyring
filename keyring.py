12a296a8bb14f92feb1e194b51750395b2fa1846
anfractuosity <anfractuosity@anfractuosity.com>
2022-06-20 20:59:17 +0100

    Refactor, so that image size determines size of object

--- keyring.py
+++ keyring.py
@@ -1,4 +1,4 @@
-import Draft
+
 import numpy as np
 import Part
 import ProfileLib.RegularPolygon
@@ -6,35 +6,24 @@ from FreeCAD import Base
 from PIL import Image
 
 def test(image, outputprefix, drillA=True):
-    rheight = 80
-    rwidth = 30
-    thick = 2
-
-    x0 = 2
-    y0 = 12
-    x1 = rwidth - 2
-    y1 = rheight - 2
-
-    h1 = y1 - y0
-    w1 = x1 - x0
-
-    hole = 0.17
-    scale = 2.0
-
-    height = int((y1 - y0)*scale)
-    width = int((x1 - x0)*scale)
-
     img = Image.open(image)
     img = img.convert('L')
     arr = np.array(img) > 0
     arr = np.swapaxes(arr, 0, 1)
 
-    iwidth = (width // 2) + 1
-    iheight = (height // 2) + 1
-    print(f"Please use image of size - {iwidth}x{iheight}")
+    hole = 0.5 # Radius of hole
+    gap = 0.5  # Gap between holes
+    border = 3
+
+    rheight = (arr.shape[0] * 2 * ((hole*2)+gap))
+    rwidth = (arr.shape[1] * 2 * ((hole*2)+gap))
+    thick = 1
+
+    height = arr.shape[0] * 2
+    width = arr.shape[1] * 2
 
     np.random.seed(1)
-    arr2 = np.random.randint(2, size=(iheight, iwidth)) > 0
+    arr2 = np.random.randint(2, size=(height, width)) > 0
 
     doc = FreeCAD.newDocument()
     myPart = doc.addObject("Part::Feature","myPartName")
@@ -47,33 +36,31 @@ def test(image, outputprefix, drillA=True):
     B[0][1] = True
     B[1][0] = True
 
-    cube = FreeCAD.ActiveDocument.addObject("Part::Feature", "myCube")
-    cube.Shape = Part.makeBox(rheight, rwidth, thick)
+    print(f"{rheight + border},{rwidth + border}")
+    cube = doc.addObject("Part::Feature", "myCube")
+    cube.Shape = Part.makeBox(rheight + border, rwidth + border, thick)
 
-    chmfr = FreeCAD.ActiveDocument.addObject("Part::Fillet", "myChamfer")
-    chmfr.Base = FreeCAD.ActiveDocument.myCube
+    chmfr = doc.addObject("Part::Fillet", "myChamfer")
+    chmfr.Base = doc.myCube
     myEdges = []
-
     for i in range(1, 12+1):
         myEdges.append((i, 0.35, 0.325)) # (edge number, chamfer start length, chamfer end length)
 
     chmfr.Edges = myEdges
-    FreeCADGui.ActiveDocument.myCube.Visibility = False
-    FreeCAD.ActiveDocument.recompute()
+    doc.myCube.Visibility = False
+    doc.recompute()
 
-    doc.addObject('PartDesign::Body','Body001')
-    SocketSketch = doc.Body001.newObject('Sketcher::SketchObject','SketchHole0')
-    SocketSketch.Support = (chmfr,["Face14"])
+    SocketSketch = doc.addObject('Sketcher::SketchObject','SketchHole0')
+    SocketSketch.Support = (chmfr, ["Face14"])
     SocketSketch.MapMode = 'FlatFace'
     doc.recompute()
 
-    for y in range(0, height+1):
-        for x in range(0, width+1):
+    for y in range(0, height):
+        for x in range(0, width):
             color = arr[y//2][x//2]
             choice = arr2[y//2][x//2]
             x_ = x % 2
             y_ = y % 2
-
             if choice:
                 drill = A[y_][x_]
             else:
@@ -87,19 +74,18 @@ def test(image, outputprefix, drillA=True):
                 drillb = drill
 
             if (drillA and drilla) or ((not drillA) and drillb):
-                y_ = y0 + ((y/height)*h1)
-                x_ = x0 + ((x/width)*w1)
+                y_ = hole + ((y/height) * rheight) + (border/2)
+                x_ = hole + ((x/width) * rwidth) + (border/2)
                 SocketSketch.addGeometry(Part.Circle(App.Vector(y_, x_, 0), App.Vector(0,0,1), hole), False)
 
-    SocketSketch.addGeometry(Part.Circle(App.Vector(4, rwidth / 2, 0), App.Vector(0,0,1), 1.5),False)
-
-    pocket = doc.Body001.newObject("PartDesign::Pocket","Pocket0")
+    #SocketSketch.addGeometry(Part.Circle(App.Vector(8, rwidth / 2, 0), App.Vector(0,0,1), 1.5),False)
+    pocket = doc.addObject("PartDesign::Pocket","Pocket0")
     pocket.Profile = SocketSketch
-    pocket.Length = 5.0
+    pocket.Length = 10
+
     Gui.activeDocument().hide("SketchHole0")
     doc.recompute()
 
-    Gui.activeDocument().hide("SketchHole")
     Gui.activeDocument().hide("myPartName")
     Gui.activeDocument().hide("myChamfer")
     doc.recompute()
